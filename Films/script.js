5f1e1e3b9d7790d3adab273f599df75d4a03b2d9
Mihkel <mikhkel@gmail.com>
2021-11-12 15:42:20 +0300

    first Film  commit

---
+++ Films/script.js
@@ -0,0 +1,19 @@
+const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    acters: {},
+    genres: [],
+    privat: false
+};
+
+let a = prompt('Один из просмотренных фильмов', ''),
+    b = prompt('На сколько оцените его?',''),
+    c = prompt('Один из просмотренных фильмов', ''),
+    d = prompt('На сколько оцените его?','');
+
+ personalMovieDB.movies[a] = b;
+ personalMovieDB.movies[c] = d;
+
+ console.log(personalMovieDB);
