5f92daf119ec118795f18da605397cfb84a8a9a4
Mihkel Alatalo <alatalo@gmail.com>
2021-11-16 00:07:41 +0300

    ночь понедельника

--- Methods/script.js
+++ Methods/script.js
@@ -14,4 +14,14 @@ console.log(fruit.indexOf('fruit'));
 
 const logg = 'Hello World';
 
-console.log(logg.slice(-6));
+console.log(logg.slice(-2));
+console.log(logg.substring(6, 10));
+console.log(logg.substr(6, 2));
+
+const numse = 22.2;
+console.log(Math.round(numse));
+
+const test = '12.22px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
+
