f70b3152783be2d62f7ce984fdf9989ddbdfbee9
Mihkel <mikhkel@gmail.com>
2021-11-15 13:12:05 +0300

    added Cicles folder

--- If/script.js
+++ If/script.js
@@ -1,7 +1,27 @@
 "use sfrict";
 
-if (4 == "4") {
-    console.log('Ok!');
-}else{
-    console.log('No!');
+// if (4 == "4") {
+//     console.log('Ok!');
+// }else{
+//     console.log('No!');
+// }
+
+// let num = 50;
+// (num == 50) ? console.log('Ok!'): console.log('Error!');
+
+
+const num = 101;
+switch (num) {
+    case 49:
+        console.log('Error!');
+        break;
+    case 100:
+        console.log('Error!');
+        break;
+    case 50:
+        console.log('ESS!');
+        break;
+    default:
+        console.log('Не в этот раз!');
+        break;
 }
