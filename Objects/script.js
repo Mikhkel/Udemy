f66f943a73e535415043dcb355ca37283ba34964
Mihkel <mikhkel@gmail.com>
2021-11-16 18:18:11 +0300

    Вторник-2

--- Objects/script.js
+++ Objects/script.js
@@ -10,8 +10,15 @@ const options = {
     }
 };
 
+// delete options.colors;
 
+// console.log(options);
 
-delete options.colors;
-
-console.log(options);
+for (let key in options) {
+    if (typeof(options[key]) === 'object'){
+        for (let i in options[key]){
+            
+        }
+    }
+    console.log(`Свойство ${key} имеет значение ${options[key]}`);
+}
